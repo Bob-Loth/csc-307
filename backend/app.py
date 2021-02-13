6d774f438fefc5aba826447472876278b5eb54c2
Bob Loth <bob94loth@gmail.com>
2021-02-13 15:18:07 -0800

    Added login functionality to backend. Notes below.

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+from model_mongodb import *
 
+from hash import *
+from utils.Verification import verify_password
+
+ 
 app = Flask(__name__)
 CORS(app)
+ 
 
 @app.route('/')
 def home():
@@ -13,11 +19,23 @@ def home():
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
-        name = request.get_json().get('name')
-        passwd = request.get_json().get('pwd')
-        #TODO: do password encryption here before sending to database
-        #TODO: check if hashed password matches user in collection 'users'
-            #if not, return response indicating unsuccessful login
-        #TODO: return response indicating successful login, redirect
-        resp = jsonify(username=name, password=passwd)
+        login_attempt = request.get_json()
+        login = Login(login_attempt)
+        
+        # TODO: do password encryption here before sending to database
+        # TODO: check if hashed password matches user in collection 'users'
+        # if not, return response indicating unsuccessful login
+        db_hash = login.find_name_ret_hash(login_attempt.get('name'))
+        if db_hash == False: 
+            resp = jsonify({"success": "false"})
+            return resp
+        
+        match = verify(login_attempt.get('pwd'), db_hash)
+        
+        if match:
+            resp = jsonify({"success": "true"})
+        else:
+            resp = jsonify({"success": "false"})
+        
+        # TODO: return response indicating successful login, redirect
         return resp
---
+++ backend/private_credentials.py
@@ -0,0 +1,6 @@
+def credentials():
+  username = "" #insert mongodb username here
+  password = "" #insert mongodb password here
+  
+  cred = 'mongodb+srv://' + username + ':' + password + '@inventorydb.zhcq6.mongodb.net'
+  return cred
