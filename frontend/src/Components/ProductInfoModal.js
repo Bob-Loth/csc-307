70b946591d3af10fadf263a94c323f7fe6af372f
Bob Loth <bob94loth@gmail.com>
2021-02-22 10:37:06 -0800

    tracked modal

---
+++ frontend/src/Components/ProductInfoModal.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+import axios from 'axios'
+import React, {useState} from 'react'
+
+import { Button, Header, Modal } from 'semantic-ui-react'
+
+function ProductInfoModal(props){
+  const [open, setOpen] = useState(false);
+  return(
+    <Modal
+    onClose={() => setOpen(false)}
+    onOpen={() => setOpen(true)}
+    open={open}
+    trigger={<Button fluid>Edit<br></br>{props.name}</Button>}>
+
+      <Modal.Header>Product Info:</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <Header>Name: {props.name}</Header>
+          <p>Expiration date: {props.expiration_date}</p>
+          <p>SKU: {props.sku}</p>
+          <p>Category: {props.category}</p>
+          <p>Price: ${props.price}</p>
+          <p>Shipment Batch: {props.shipment_batch}</p>
+        </Modal.Description>
+      </Modal.Content>
+
+      <Modal.Actions>
+        <Button
+        onClick={() => setOpen(false)}>
+          Exit Without Changes
+        </Button>
+        <Button
+        onClick={() => setOpen(false)}> 
+          Confirm Changes and Exit
+        </Button>
+      </Modal.Actions>
+     
+    </Modal>
+  )
+}
+
+export default ProductInfoModal
