4ce7dae20a3a1017aaa2bce0b97556628bafcb6f
David N <nguyen.david94579@gmail.com>
2021-02-22 10:57:25 -0800

    Made App.js to seperate component for testing, index.js renders App.js

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from "../index";
+import Login from "../Pages/login";
 
+// test('error object from failed ', () => {
+//   render(<App />);
+//   const loginTextback = screen.getByText(login);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
+test('calls onClick prop when clicked', () => {
+  const handleClick = jest.fn()
+  render(<Login />)
+  fireEvent.click(screen.getByText(/Login/i))
+  expect(loginUserCallback).toHaveBeenCalledTimes(1)
+})
