822d1b40398965d3861b8e16dbe4cc6dbcb5e917
David N <nguyen.david94579@gmail.com>
2021-02-28 14:48:11 -0800

    Added simple frontend test.

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -1,11 +1,7 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-
-import App from "../App";
-import Login from "../Pages/login";
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 
 
-// test('testing failed login no input', () => {
-//   render(<Login />)
-//   fireEvent.click(screen.getByText('Login!') )
-//   expect(screen.getByText(/Login Failed!/).toBeInTheDocument() )
-// })
+test('example test description', () => {
+    expect( true )
+})
