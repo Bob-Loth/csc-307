4d191015285fff79b4c07b41c278d7213ac0d43f
Bob Loth <bob94loth@gmail.com>
2021-03-02 16:55:29 -0800

    removed nested test function

--- backend/test_model_mongodb.py
+++ backend/test_model_mongodb.py
@@ -2,8 +2,7 @@ import pytest
 from model_mongodb import *
 
 
-def test_find_all():
-    def test_inventory(mongodb):
-        assert 'InventoryColl' in mongodb.list_collection_names()
-        grapes = mongodb.players.find_one({'name': 'Grapes'})
-        assert grapes['category'] == 'Produce'
+def test_inventory(mongodb):
+    assert 'InventoryColl' in mongodb.list_collection_names()
+    grapes = mongodb.InventoryColl.find_one({'name': 'Grapes'})
+    assert grapes['category'] == 'Produce'
