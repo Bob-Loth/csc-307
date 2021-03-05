3c90a9d286b964aa0a9ee2c00a45855e38f8bc4f
Bob-Loth <bob94loth@gmail.com>
2021-03-04 22:41:48 -0800

    CI testing integration

---
+++ backend/private_credentials.py
@@ -0,0 +1,6 @@
+def credentials():
+    username = "Travis"  # insert mongodb username here
+    password = "uYwUwNC8Xi8vSN0a"  # insert mongodb password here
+    cred = ('mongodb+srv://' + username + ':' +
+            password + '@inventorydb.zhcq6.mongodb.net')
+    return cred
