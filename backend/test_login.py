e2723e63737e4a0e40ecd2273020e4b4850711a4
Bob-Loth <bob94loth@gmail.com>
2021-03-05 10:52:33 -0800

    added login tests under test_login.py

---
+++ backend/test_login.py
@@ -0,0 +1,47 @@
+import requests
+from flask import jsonify
+import pymongo
+import json
+from bson.objectid import ObjectId
+from private_credentials import credentials
+
+
+base_url = 'http://localhost:5000/'
+header = {'content-type': 'application/json'}
+
+
+class TestLogin():
+    # tests to see that a failure is returned when logging in with a
+    # nonexistent user
+    def test_login_failure_non_existent(self):
+        data_to_send = {'name': 'NotHere-abhwergtdfd', 'pwd': 'aErgUYbjhertJT'}
+        r = requests.post(base_url + 'login', data=json.dumps(data_to_send),
+                          headers=header)
+        assert r.status_code == 401
+        assert r.json()['success'] is False
+        assert r.json()['errors']['username'] == []
+        assert r.json()['errors']['password'] == []
+        assert r.json()['hash'] == 'false'
+
+    # user "User" exists in database, but incorrect, valid
+    # password was provided.
+    # Tests to see if this correctly returns an error.
+    def test_login_failure_incorrect_password(self):
+        data_to_send = {'name': 'User', 'pwd': 'not-password'}
+        r = requests.post(base_url + 'login', data=json.dumps(data_to_send),
+                          headers=header)
+        assert r.status_code == 401
+        assert r.json()['success'] is False
+        assert r.json()['errors']['username'] == []
+        assert r.json()['errors']['password'] == []
+        assert r.json()['verify'] == 'false'
+
+    def test_login_success(self):
+        data_to_send = {'name': 'User', 'pwd': 'password'}
+        r = requests.post(base_url + 'login', data=json.dumps(data_to_send),
+                          headers=header)
+        assert r.status_code == 200
+        assert r.json()['success'] is True
+        assert r.json()['errors']['username'] == []
+        assert r.json()['errors']['password'] == []
+        assert r.json()['jwtToken']  # checks for the existence of a token sent
