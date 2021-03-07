92926ae75e17a8f59948f9e32858c962b61f2062
Bob-Loth <bob94loth@gmail.com>
2021-03-07 14:23:47 -0800

    fixed test_update, renamed it to _success, and added a _failure test for it as well

--- backend/test_search.py
+++ backend/test_search.py
@@ -28,25 +28,37 @@ class TestSearch():
         assert len(r.json()['products']) == 20
         assert r.status_code == 200
 
-    def test_update_one(self):
-        data_to_send = {'name': 'Dishwashing Machine'}
-        r = requests.patch(base_url + 'search', 
+    # verifies that multiple fields can be changed with one patch request,
+    # and that the returned JSON contains the modified object.
+    def test_update_one_success(self):
+        data_to_send = {'name': 'Dishwashing Machine', 'price': 6.00}
+        r = requests.patch(base_url + 'search?_id=603dc23efb89b823bcc0ce98',
                            data=json.dumps(data_to_send),
-                           params={'_id': \
-                                   "603dc23efb89b823bcc0ce98"},
                            headers=header)
-        print(r.text)
-        assert r.text
-        assert r.status_code == 205
 
-        data_to_send2 = 'Dishwasher'
-        r2 = requests.patch(base_url + 'search', 
-                           data=json.dumps(data_to_send2),
-                           params={'_id': \
-                                   "603dc23efb89b823bcc0ce98"},
-                           headers=header)
-        
-        assert r2.json()['name'] == 'Dishwasher'
+        assert r.status_code == 205
+        assert r.json()['price'] == 6.00
+        assert r.json()['name'] == 'Dishwashing Machine'
+
+        data_to_send2 = {'name': 'Dishwasher', 'price': 999.99}
+        r2 = requests.patch(base_url + 'search?_id=603dc23efb89b823bcc0ce98',
+                            data=json.dumps(data_to_send2),
+                            headers=header)
+
         assert r2.status_code == 205
-        
-                           
+        assert r2.json()['price'] == 999.99
+        assert r2.json()['name'] == 'Dishwasher'
+
+    # verifies that if, for some reason, an incorrect _id was specified,
+    # a 404 response code and a status field is returned. As the react app
+    # does not provide any functionality to modify the _id, this error means
+    # that an external program modified the _id in between getting the table,
+    # and sending the patch request.
+    def test_update_one_failure(self):
+        data_to_send = {'name': 'Dishwashing Machine', 'price': 6.00}
+        r = requests.patch(base_url + 'search?_id=cccccccccccccccccccccccc',
+                           data=json.dumps(data_to_send),
+                           headers=header)
+
+        assert r.status_code == 404
+        assert r.json()['status'] == 'something went wrong'
