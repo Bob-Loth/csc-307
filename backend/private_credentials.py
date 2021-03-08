f012b9e05e0c013f62a9693d32ff8c1ec9245cca
Bob-Loth <bob94loth@gmail.com>
2021-03-08 11:02:31 -0800

    added stock count variable to the SearchFilter table and the modal edit component.

--- backend/private_credentials.py
+++ backend/private_credentials.py
@@ -1,6 +1,9 @@
+import os
+
+
 def credentials():
-    username = "Travis"  # insert mongodb username here
-    password = "uYwUwNC8Xi8vSN0a"  # insert mongodb password here
+    username = os.getenv("MONGO_username")  # insert mongodb username here
+    password = os.getenv("MONGO_password")  # insert mongodb password here
     cred = ('mongodb+srv://' + username + ':' +
             password + '@inventorydb.zhcq6.mongodb.net')
     return cred
