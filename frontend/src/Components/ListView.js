5d9bad7773c550df2013c58843826c7c38d555f7
Bob-Loth <bob94loth@gmail.com>
2021-03-09 21:47:40 -0800

    dashboard added

--- frontend/src/Components/ListView.js
+++ frontend/src/Components/ListView.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import {Table} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
+import { toArray } from 'lodash'
 
 function ListView({listName, secondHeaderField, products}) {
+
+
+
     return (
         <div>
             <h1>{listName}</h1>
             <Table compact>
                 <Table.Header>
+                    <Table.Row>
                     <Table.HeaderCell>name</Table.HeaderCell>
                     <Table.HeaderCell>{secondHeaderField}</Table.HeaderCell>
+                    </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                    <Table.Row>
+                    {(products = toArray(products)) &&
+                    products.map(({_id, name, expiration_date, stock}) => (
+                    <Table.Row key={_id}>
                         <Table.Cell>
-                            {products.pName}
+                            {name}
                         </Table.Cell>
                         <Table.Cell>
-                            {products.additionalField}
+                            {((secondHeaderField === 'Stock Count') && stock) ||
+                             ((secondHeaderField === 'Expiration Date') && expiration_date)}
                         </Table.Cell>
                     </Table.Row>
+                    ))}
+
                 </Table.Body>
             </Table>
         </div>
